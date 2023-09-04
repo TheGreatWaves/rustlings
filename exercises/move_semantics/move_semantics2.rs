3eeb8c6a4e4aa3eba1d391fed4c4c499b4a12545
liv <shadows_withal@fastmail.com>
2023-09-04 14:20:37 +0200

    fix: refactor move semantics 1-4 into tests

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -1,18 +1,18 @@
 // move_semantics2.rs
-// Make me compile without changing line 17 or moving line 14!
-// Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
-
-// Expected output:
-// vec0 has length 3, with contents `[22, 44, 66]`
-// vec1 has length 4, with contents `[22, 44, 66, 88]`
+// Make the test pass by finding a way to keep both Vecs separate!
+//
+// Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
+// for a hint.
 
 // I AM NOT DONE
 
+#[test]
 fn main() {
-    let vec0 = Vec::new();
+    let vec0 = vec![22, 44, 66];
 
     let mut vec1 = fill_vec(vec0);
 
+<<<<<<< HEAD
     println!(
         "{} has length {}, with contents: `{:?}`",
         "vec0",
@@ -28,14 +28,16 @@ fn main() {
         vec1.len(),
         vec1
     );
+=======
+    assert_eq!(vec0, vec![22, 44, 66]);
+    assert_eq!(vec1, vec![22, 44, 66, 88]);
+>>>>>>> 51e237d (fix: refactor move semantics 1-4 into tests)
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    vec.push(88);
 
     vec
 }

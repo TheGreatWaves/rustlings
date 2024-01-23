9a9fc3bd27b69c9ce5b30a96d222f8540bca679b
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2024-01-23 11:45:30 +0700

    merged

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -5,10 +5,12 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8, u8, u8),
+    Move(Point),
+    Quit,
+    Echo(String),
 }
 
 struct Point {
